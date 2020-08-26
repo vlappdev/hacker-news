604352e69035d636e6e7a8ec3ef9cdfd8636c0d1
vlappdev <vladimir.eoffice@gmail.com>
2020-08-26 12:06:05 +0200

    Updates

---
+++ precache-manifest.62c86f718a62bf3833698a7e1dc9ff41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130bb57b227ea19135ecf3903e66a5cb",
+    "url": "/hacker-news/index.html"
+  },
+  {
+    "revision": "3f42306e2ebae50e849a",
+    "url": "/hacker-news/static/css/main.7a4c9581.chunk.css"
+  },
+  {
+    "revision": "4acd3a84f5f22cde1972",
+    "url": "/hacker-news/static/js/2.f43d4aeb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hacker-news/static/js/2.f43d4aeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f42306e2ebae50e849a",
+    "url": "/hacker-news/static/js/main.55acfd51.chunk.js"
+  },
+  {
+    "revision": "f934003405a453338133",
+    "url": "/hacker-news/static/js/runtime-main.bdc9e62f.js"
+  }
+]);
